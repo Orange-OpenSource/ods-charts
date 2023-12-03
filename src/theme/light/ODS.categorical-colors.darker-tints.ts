05aa682b705778ade7cc1206dff3fa6a9631b067
Julien Déramond <julien.deramond@orange.com>
2023-12-03 15:32:28 +0100

    fix(lib): 'DAKER*' → 'DARKER' typo

--- src/theme/light/ODS.categorical-colors.darker-tints.ts
+++ src/theme/light/ODS.categorical-colors.darker-tints.ts
@@ -6,6 +6,6 @@
 // This software is distributed under the MIT license.
 //
 
-export const LIGHT_CATEGORICAL_COLORS_DAKER_TINTS = {
+export const LIGHT_CATEGORICAL_COLORS_DARKER_TINTS = {
   color: ['#085EBD', '#0A6E31', '#FF8AD4', '#492191', '#FFB400'],
 };
