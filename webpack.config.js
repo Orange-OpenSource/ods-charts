291cb6481f29774ea1b03c3478d42f892d7379e5
Louis-Maxime Piton <louismaxime.piton@orange.com>
2024-04-25 14:32:41 +0200

    Minified version of the lib

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,74 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  entry: {
-    main: './index.ts',
+module.exports = [
+  {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    entry: {
+      main: './index.ts',
+    },
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: 'ods-charts.js',
+      libraryTarget: 'umd',
+      library: 'ODSCharts',
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: 'ts-loader',
+        },
+      ],
+    },
   },
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'ods-charts.js',
-    libraryTarget: 'umd',
-    library: 'ODSCharts',
+  {
+    mode: 'development',
+    devtool: false,
+    entry: {
+      main: './index.ts',
+    },
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: 'ods-charts.min.js',
+      libraryTarget: 'umd',
+      library: 'ODSCharts',
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: 'ts-loader',
+        },
+      ],
+    },
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            format: {
+              comments: false,
+              preamble: `/*!
+  * Software Name: Orange Design System Charts
+  * SPDX-FileCopyrightText: Copyright (c) 2023 - 2024 Orange SA
+  * SPDX-License-Identifier: MIT
+  * 
+  * This software is distributed under the MIT license.
+  */
+  `,
+            },
+          },
+          extractComments: false,
+        }),
+      ],
+    },
   },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-      },
-    ],
-  },
-};
+];
