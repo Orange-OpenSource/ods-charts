0835ecfdaf61bd913c60430fde9e901b99487711
Jacques Lebourgeois <jacques.lebourgeois@orange.com>
2025-03-28 15:10:41 +0100

    Update colorIsDark method

--- docs/colors/index.js
+++ docs/colors/index.js
@@ -6,7 +6,15 @@ function colorIsDark(bgColor) {
   let r = parseInt(color.substring(0, 2), 16); // hexToR
   let g = parseInt(color.substring(2, 4), 16); // hexToG
   let b = parseInt(color.substring(4, 6), 16); // hexToB
-  return r * 0.299 + g * 0.587 + b * 0.114 <= 186;
+  let uicolors = [r / 255, g / 255, b / 255];
+  let c = uicolors.map((col) => {
+    if (col <= 0.03928) {
+      return col / 12.92;
+    }
+    return Math.pow((col + 0.055) / 1.055, 2.4);
+  });
+  let L = 0.2126 * c[0] + 0.7152 * c[1] + 0.0722 * c[2];
+  return L <= 0.179;
 }
 
 function displayColorSet(containerId, colorSet, colorSetLabel) {
