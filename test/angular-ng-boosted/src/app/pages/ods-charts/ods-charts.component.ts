2317f2603dc21829f59d7b657b3ca4b19ce2a345
Jacques Lebourgeois <jacques.lebourgeois@orange.com>
2025-03-31 15:24:12 +0200

    Simplify and make angular-ng-boosted compile

--- test/angular-ng-boosted/src/app/pages/ods-charts/ods-charts.component.ts
+++ test/angular-ng-boosted/src/app/pages/ods-charts/ods-charts.component.ts
@@ -7,11 +7,11 @@ import { NgxEchartsDirective, provideEchartsCore } from 'ngx-echarts';
 import { ColorModeService } from '@services/color-mode.service';
 import * as echarts from 'echarts/core';
 import { BarChart, LineChart } from 'echarts/charts';
-import { DatasetComponent, GridComponent } from 'echarts/components';
+import { DatasetComponent, GridComponent, TitleComponent } from 'echarts/components';
 import { CanvasRenderer } from 'echarts/renderers';
 import { LegendComponent } from 'echarts/components';
 import { TooltipComponent } from 'echarts/components';
-echarts.use([BarChart, GridComponent, CanvasRenderer, LegendComponent, TooltipComponent, LineChart, DatasetComponent]);
+echarts.use([BarChart, GridComponent, CanvasRenderer, LegendComponent, TooltipComponent, LineChart, DatasetComponent, TitleComponent]);
 
 @Component({
   selector: 'app-ods-charts',
@@ -51,44 +51,20 @@ export class OdsChartsComponent implements OnInit {
   };
   public updateOptions1: EChartsOption = {};
 
-  public options2: EChartsOption = {
-    legend: {},
-    tooltip: {},
-    dataset: {
-      // Provide a set of data.
-      source: [
-        ['year', 'Matcha Latte', 'Milk Tea', 'Cheese Cocoa', 'Walnut Brownie'],
-        ['2015', 43.3, 83.1, 86.4, 72.4],
-        ['2016', 85.8, 73.4, 65.2, 53.9],
-        ['2017', 93.7, 55.1, 82.5, 39.1],
-      ],
-    },
-    // Declare an x-axis (category axis).
-    // The category map the first column in the dataset by default.
-    xAxis: { type: 'category', data: ['2015', '2016', '2017'] },
-    // Declare a y-axis (value axis).
-    yAxis: {},
-    // Declare several 'bar' series,
-    // every series will auto-map to each column by default.
-    series: [{ type: 'bar' }, { type: 'bar' }, { type: 'bar' }, { type: 'bar' }],
-  };
+  public options2: EChartsOption = {};
   public updateOptions2: EChartsOption = {};
 
-  constructor(private colorModeService: ColorModeService) {}
+  constructor() {}
 
   ngOnInit(): void {
-    this.colorModeService.currentMode.subscribe((value) => {
-      if (this.currentMode !== value) {
-        this.currentMode = value;
-        this.generateThemes();
-      }
-    });
     this.generateThemes();
   }
 
   public onChart1Init(ec: echarts.ECharts) {
     this.options1 = this.myTheme1
       .setDataOptions(this.options1)
+      .manageThemeObserver(ec)
+      .manageChartResize(ec, 'chart1')
       .externalizeLegends(ec, '#legend1')
       .externalizePopover({}, ODSCharts.ODSChartsPopoverManagers.BOOSTED5)
       .getChartOptions();
@@ -96,7 +72,29 @@ export class OdsChartsComponent implements OnInit {
 
   public onChart2Init(ec: echarts.ECharts) {
     this.options2 = this.myTheme2
-      .setDataOptions(this.options2)
+      .setDataOptions({
+        legend: {},
+        tooltip: {},
+        dataset: {
+          // Provide a set of data.
+          source: [
+            ['year', 'Matcha Latte', 'Milk Tea', 'Cheese Cocoa', 'Walnut Brownie'],
+            ['2015', 43.3, 83.1, 86.4, 72.4],
+            ['2016', 85.8, 73.4, 65.2, 53.9],
+            ['2017', 93.7, 55.1, 82.5, 39.1],
+          ],
+        },
+        // Declare an x-axis (category axis).
+        // The category map the first column in the dataset by default.
+        xAxis: { type: 'category', data: ['2015', '2016', '2017'] },
+        // Declare a y-axis (value axis).
+        yAxis: {},
+        // Declare several 'bar' series,
+        // every series will auto-map to each column by default.
+        series: [{ type: 'bar' }, { type: 'bar' }, { type: 'bar' }, { type: 'bar' }],
+      })
+      .manageThemeObserver(ec)
+      .manageChartResize(ec, 'chart2')
       .externalizeLegends(ec, '#legend2')
       .externalizePopover({}, ODSCharts.ODSChartsPopoverManagers.BOOSTED5)
       .getChartOptions();
