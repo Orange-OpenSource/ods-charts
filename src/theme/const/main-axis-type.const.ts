7a0972f43068458da47fefe002ecdd05d4dbecad
Jacques Lebourgeois <jacques.lebourgeois@orange.com>
2025-04-30 15:18:05 +0200

    fix(doc): Update licence dates

--- src/theme/const/main-axis-type.const.ts
+++ src/theme/const/main-axis-type.const.ts
@@ -1,3 +1,11 @@
+//
+// Software Name: Orange Design System Charts
+// SPDX-FileCopyrightText: Copyright (c) 2023 - 2025 Orange SA
+// SPDX-License-Identifier: MIT
+//
+// This software is distributed under the MIT license.
+//
+
 export enum MainAxisType {
   category = 'category',
   time = 'time',
--- src/theme/default/ODS.lines.axis.ts
+++ src/theme/default/ODS.lines.axis.ts
@@ -1,6 +1,6 @@
 //
 // Software Name: Orange Design System Charts
-// SPDX-FileCopyrightText: Copyright (c) 2023 - 2024 Orange SA
+// SPDX-FileCopyrightText: Copyright (c) 2023 - 2025 Orange SA
 // SPDX-License-Identifier: MIT
 //
 // This software is distributed under the MIT license.
