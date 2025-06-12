b2d8f82b4bb3bf9c3d49f328c92ea7d5df76e22b
Louis-Maxime Piton <louismaxime.piton@orange.com>
2025-06-12 11:11:09 +0200

    Revert content.config.ts to generate the whole doc

--- docs/src/content.config.ts
+++ docs/src/content.config.ts
@@ -1,22 +1,22 @@
-// 1. Import utilities from `astro:content`
-import { defineCollection, z } from 'astro:content';
+// // 1. Import utilities from `astro:content`
+// import { defineCollection, z } from 'astro:content';
 
-// 2. Import loader(s)
-import { glob } from 'astro/loaders';
+// // 2. Import loader(s)
+// import { glob } from 'astro/loaders';
 
-// 3. Define your collection(s)
-const examples = defineCollection({
-  loader: glob({ pattern: '**/*.(md|mdx)', base: './src/content/examples' }),
-  schema: z.object({
-    title: z.string(),
-  }),
-});
-const useCases = defineCollection({
-  loader: glob({ pattern: '**/*.(md|mdx)', base: './src/content/use_cases' }),
-  schema: z.object({
-    title: z.string(),
-  }),
-});
+// // 3. Define your collection(s)
+// const examples = defineCollection({
+//   loader: glob({ pattern: '**/*.(md|mdx)', base: './src/content/examples' }),
+//   schema: z.object({
+//     title: z.string(),
+//   }),
+// });
+// const useCases = defineCollection({
+//   loader: glob({ pattern: '**/*.(md|mdx)', base: './src/content/use_cases' }),
+//   schema: z.object({
+//     title: z.string(),
+//   }),
+// });
 
-// 4. Export a single `collections` object to register your collection(s)
-export const collections = { examples, useCases };
+// // 4. Export a single `collections` object to register your collection(s)
+// export const collections = { examples, useCases };
