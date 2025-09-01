265af6190c6657791a9c05c2fbc4979b2ad523ab
Louis-Maxime Piton <louismaxime.piton@orange.com>
2025-09-01 14:45:50 +0200

    Add config to avoid weird files

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 import { odsCharts } from './src/libs/astro';
+import { getConfig } from './src/libs/config';
 
 export default defineConfig({
+  build: {
+    assets: `${getConfig().docs_version}/assets`
+  },
   integrations: [odsCharts()],
   markdown: {
     smartypants: false,
