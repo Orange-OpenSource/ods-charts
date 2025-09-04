954e740fd5533d1782f682b1305dea3b4b2dded7
Jacques Lebourgeois <jacques.lebourgeois@orange.com>
2025-09-04 17:18:25 +0200

    fix(tests): fix angular-ng-boosted test

--- test/angular-ng-boosted/src/app/components/footer/footer.component.ts
+++ test/angular-ng-boosted/src/app/components/footer/footer.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
